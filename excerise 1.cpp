64fcc32fa0299c6d188688c755a16033a816ea00
jay-omon <jayword022@gmail.com>
2026-06-29 20:39:06 +0100

    Initial C++ project commit

---
+++ excerise 1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Ikum Victory Christopher" << endl;
+    cout << "Reg. No: 25/EG/CO/121" << endl;
+        cout << "Department: Computer Engineering" << endl;
+    return 0;
+}
